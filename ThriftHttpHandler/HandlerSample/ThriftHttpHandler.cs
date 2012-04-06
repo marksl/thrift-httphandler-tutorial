431553938660466bd42daab338ed4d07f1bce0a6
Mark Lamley <mark.lamley@gmail.com>
2012-04-06 14:18:28 -0700

    reused more code

--- ThriftHttpHandler/HandlerSample/ThriftHttpHandler.cs
+++ ThriftHttpHandler/HandlerSample/ThriftHttpHandler.cs
@@ -1,50 +1,13 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using System.Web;
-using Thrift;
-using Thrift.Protocol;
+﻿using Thrift.Protocol;
 using Thrift.Transport;
 
 namespace HandlerSample
 {
-    public class ThriftHttpHandler : IHttpHandler
+    public class ThriftHttpHandler : THttpHandler
     {
-        public void ProcessRequest(HttpContext context)
+        public ThriftHttpHandler()
+            : base(CreateProcessor(), CreateJsonFactory())
         {
-            context.Response.ContentType = "application/x-thrift";
-            context.Response.ContentEncoding = Encoding.UTF8;
-
-            ProcessRequest(context.Request.InputStream, context.Response.OutputStream);
-        }
-
-        private static void ProcessRequest(Stream input, Stream output)
-        {
-            TTransport transport = new TStreamTransport(input, output);
-
-            var protocolFactory = CreateJsonFactory();
-            var processor = CreateProcessor();
-            try
-            {
-                TProtocol inputProtocol = protocolFactory.GetProtocol(transport);
-                TProtocol outputProtocol = protocolFactory.GetProtocol(transport);
-
-                while (processor.Process(inputProtocol, outputProtocol)) { }
-            }
-            catch (TTransportException)
-            {
-                // Client died, just move on
-            }
-            catch (TApplicationException tx)
-            {
-                Console.Error.Write(tx);
-            }
-            catch (Exception x)
-            {
-                Console.Error.Write(x);
-            }
-
-            transport.Close();
         }
 
         private static Calculator.Processor CreateProcessor()
@@ -55,11 +18,6 @@ namespace HandlerSample
         private static TJSONProtocol.Factory CreateJsonFactory()
         {
             return new TJSONProtocol.Factory();
-        }
-
-        public bool IsReusable
-        {
-            get { return true; }
         }
     }
 }
