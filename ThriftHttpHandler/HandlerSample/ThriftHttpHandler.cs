121b37c35c91a02b2a0c3e23fcf5e1b09119c385
Mark Lamley <mark.lamley@gmail.com>
2012-04-06 10:15:35 -0700

    added basic handler

---
+++ ThriftHttpHandler/HandlerSample/ThriftHttpHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace HandlerSample
+{
+    public class ThriftHttpHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("Hellow World");
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
